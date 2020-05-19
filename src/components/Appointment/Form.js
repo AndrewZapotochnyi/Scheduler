3233cbe2e392787b43401069072afdbd85706eac
Andrew Zapotochnyi <andrewzapotochnyi@icloud.com>
2020-05-18 20:30:26 -0400

    Tests added

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -8,6 +8,7 @@ export default function Form(props) {
   
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   function save () {
     props.onSave(name, interviewer);
@@ -23,6 +24,14 @@ export default function Form(props) {
     props.onCancel();
   }
   
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+  
+    props.onSave(name, interviewer);
+  }
 
   return (<main className="appointment__card appointment__card--create">
   <section className="appointment__card-left">
@@ -34,12 +43,14 @@ export default function Form(props) {
         placeholder="Enter Student Name"
         value={name}
         onChange={event => setName(event.target.value)}
+        data-testid="student-name-input"
         
         /*
           This must be a controlled component
         */
       />
     </form>
+    <section className="appointment__validation">{error}</section>
     <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
   </section>
   <section className="appointment__card-right">
